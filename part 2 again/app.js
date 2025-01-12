dc9e49f6b83f301da507816712ab35a768dcbda3
Harshiitt26 <harshiitt26@gmail.com>
2025-01-12 18:34:30 +0530

    Login page Setup

--- part 2 again/app.js
+++ part 2 again/app.js
@@ -3,13 +3,14 @@ require('dotenv').config()
 const userRouter = require("./routes/user.routes")
 const connectDB = require("./config/db.js")
 connectDB()
-
+const cookieParser = require("cookie-parser")
 
 
 const app = express()
 app.set("view engine","ejs")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 
 app.use("/user",userRouter);
 
--- part 2 again/routes/user.routes.js
+++ part 2 again/routes/user.routes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const {body, validationResult}= require("express-validator")
 const userModel = require("../models/user.models.js")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+
 
 router.get(("/register"),(req,res)=>{
     res.render("register")
 })
+
 router.post(("/register"),
  body("username").trim().isLength({min:3}),
  body("email").trim().isLength({min:13}),
@@ -32,9 +36,44 @@ async(req,res)=>{
 })
 
 
-// router.get(("/"),(req,res)=>{
-//     res.send("User route")
-// })
+router.get(("/login"),(req,res)=>{
+    res.render("login")
+})
+router.post(("/login"),
+body("username").trim().isLength({min:3}),
+body("password").trim().isLength({min:3}),
+async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            error: errors.array(),
+            message: "Inavlid data"
+        })
+    }
+    const {username, password}= req.body;
+    const user = await userModel.findOne({
+        username: username
+    })
+    if(!user){
+        return res.status(400).json({
+            message: "Username doesnot exists"
+        })
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch){
+        return res.status(400).json({
+            message: "Username or Password is Incorrect"
+        })
+    }
+    const token = jwt.sign({
+        userId : user._id,
+        email: user.email,
+        username: user.username
+    },process.env.JWT_SECRET)
+    // res.json({token})
+    res.cookie("token",token)
+    res.send("Logged in")
+})
 // router.get(("/tests"),(req,res)=>{
 //     res.send("User tests route")
 // })
